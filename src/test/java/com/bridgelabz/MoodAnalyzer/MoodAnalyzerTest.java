629068fa27eba2019aba5128aea2efb9e6b70074
komalarao <manoharvakamullu1999@gmail.com>
2022-12-09 02:58:41 -0800

    add: proram to display message using null exception test case

---
+++ src/test/java/com/bridgelabz/MoodAnalyzer/MoodAnalyzerTest.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.MoodAnalyzer;
+
+
+	import org.junit.Assert;
+	import org.junit.Test;
+
+	public class MoodAnalyzerTest {
+	    @Test
+	    public void testMoodAnalysis_whenMoodIsSad() {
+	        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a sad message");//Tc1.1 Refactor
+	        String mood = moodAnalyzer.analyseMood();
+	        Assert.assertEquals(mood,"SAD");
+	    }
+
+	    @Test
+	    public void testMoodAnalysis_whenMoodIsHappy() {//Tc1.2 Refactor
+	        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a happy message");
+	        String mood1 = moodAnalyzer.analyseMood();
+	        Assert.assertEquals(mood1,"HAPPY");
+	    }
+	    @Test
+	    public void testMoodAnalysis_whenMoodIsNull() { //tc 2.1
+	        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+	        String mood2 = moodAnalyzer.analyseMood();
+	        Assert.assertEquals(mood2,"HAPPY");
+	    }
+	}
+
